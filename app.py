f99e7b37f3ee08a3754df37d29ce0db5c394dc44
Larissa <larissacoelhomoreira515@gmail.com>
2026-05-12 10:39:03 -0300

    adiciona Procfile

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Missão Artemis está no ar 🚀"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
